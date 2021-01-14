468a4f49ae46db1745d8ec7550ea2a5bc80b4384
Henry Wilt <henrywwilt@gmail.com>
2021-01-13 21:41:12 -0500

    Step 65

--- texty.c
+++ texty.c
@@ -195,10 +195,9 @@ int getWindowSize(int *rows, int *cols){
 
     if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
         if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12){
-            return getCursorPosition(rows, cols);
+            return -1;
         }
-        editorReadKey();
-        return -1;
+        return getCursorPosition(rows, cols);
     }
     else{
         *cols = ws.ws_col;
